edb001629ae45b8134bb5348136cd4a01900805e
Yusufkr03 <yusufkr2903@gmail.com>
2024-11-05 21:04:40 +0300

    Add files via upload

---
+++ Odev3-Soru4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int b = 1;
+int a = 1;
+int sonuc = 0;
+Console.WriteLine("Bir sayı giriniz");
+int say = int.Parse(Console.ReadLine());
+for (int i = 0; i < say; i++)
+{
+    Console.Write(sonuc+ " ");
+    a = b;
+    b = sonuc;
+    sonuc = a + b;
+    
+}
